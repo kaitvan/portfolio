e4e728e3bff7ebfff3cd39a9de1a24378986177c
Kaitlyn Van Gorkom <kaitlyn.vangorkom@gmail.com>
2023-09-25 11:56:27 -0500

    Set up basic nav and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./pages/Root";
+import HomePage from "./pages/Home";
+import AboutPage from "./pages/About";
+import PortfolioPage from "./pages/Portfolio";
+import ContactPage from "./pages/Contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/about", element: <AboutPage /> },
+      { path: "/portfolio", element: <PortfolioPage /> },
+      { path: "/contact", element: <ContactPage /> },
+    ],
+  },
+]);
+
 const App = () => {
-  return <></>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
